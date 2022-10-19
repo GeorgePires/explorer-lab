68e700da347fae9e98e0b55683d2379cd9bd0c0f
GeorgePires <georgeasp.pi@gmail.com>
2022-10-18 21:58:46 -0300

    :sparkles: Aula01/ function setCardType

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import "./css/index.css"
+
+// Pegando elementos HTML - copy selector
+const ccBgColor01 = document.querySelector('.cc-bg svg > g g:nth-child(1) path')
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+// Substituindo atributo com a var = type
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F26", "#C69347"],
+    default: ["black", "gray"]
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute('src', `cc-${type}.svg`)
+}
+
+// Disponibiliza funções globais "globalThis.my_function"
+globalThis.setCardType = setCardType("default")
