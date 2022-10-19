2e0fafd122b1098835ab0d0a113390e0c518accf
GeorgePires <georgeasp.pi@gmail.com>
2022-10-19 17:39:44 -0300

    :sparkles: Aula02/ add card-maestro

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F26", "#C69347"],
+    maestro: ["#CC2131", "#3A9BD9"],
     default: ["black", "gray"]
   }
 
@@ -61,6 +62,11 @@ const cardNumberPattern = {
       mask: "0000 0000 0000 0000",
       regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: "maestro",
     },
     {
       mask: "0000 0000 0000 0000",
